6ebc76d586f20911cef14df8e4fe7e833696e640
Wesley Pyburn (TechNobo) <10319195+TcNobo@users.noreply.github.com>
2019-02-14 18:42:07 +0200

    Init v1.0
    First file init.
    NVEncC 4.31. Nvidia Driver 418.81 or later required.

---
+++ NVEncC.bat
@@ -0,0 +1,90 @@
+@ECHO OFF
+:: Load variables
+CALL settings.bat
+:: Static variables. DO NOT EDIT
+SET batVer=1.0
+SET nvenccVer=4.31
+SET minNV=418.81
+
+:: Checks for arguments, like -h or --help
+IF "%~1"=="-h" (goto help)
+IF "%~1"=="--help" (goto help)
+
+SET /p inF="Drag and Drop input file into here: "
+ECHO.
+:: Sets outF to the output file, with the format defined in settings.bat
+IF DEFINED outFLD (goto :FolderGiven) ELSE (goto :NoFolderGiven)
+
+:fgReturn
+
+
+:: Checking if 32 or 64 bit, if not set prior.
+IF NOT DEFINED bit (
+    REG Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || SET OS=64BIT
+    IF %OS%==32BIT SET bit=32
+    IF %OS%==64BIT SET bit=64
+)
+
+:: Setting executable to the correct version
+IF "%bit%"=="32" (set nvexe=NVEncC.exe) ELSE (set nvexe=NVEncC64.exe)
+
+:: Enter respective version's folder.
+CD x%bit%
+ECHO Using NVEncC x%bit%
+ECHO The file will save to: %outF%
+ECHO If you want it saved to a new directory, exit, and set it in settings.bat.
+PAUSE
+ECHO.
+ECHO %inF% will render with the current settings:
+ECHO Resolution: %res%
+ECHO FPS: %fps%
+ECHO Codec: %codec%
+ECHO.
+ECHO This command will be run:
+ECHO.
+
+SET comStr=%nvexe% -i %inf% --%decodemode% --audio-copy --codec %codec% --fps %fps% --output-res %res% --profile %profile% --level %level% --preset %preset% --lookahead %lookahead% --cuda-schedule %cudasch% --%bitrate% --gop-len %GOPLen% --bframes %bframes% --ref %ref% --mv-precision %mvpr% --colormatrix %cm% --output "%outF%"
+IF DEFINED sar (set comStr=%comStr% --sar %sar%)
+IF DEFINED cabac (set comStr=%comStr% --cabac)
+IF DEFINED deblock (
+    IF "%deblock%"=="on" (set comStr=%comStr% --deblock)
+    IF "%deblock%"=="off" (set comStr=%comStr% --no-deblock)
+)
+
+echo %comStr%
+
+echo.
+echo.
+SET /p corr="Is this correct? (y/n): "
+IF "%corr%"=="y" (%comStr%)
+IF "%corr%"=="n" (echo Stopped.)
+
+CD ../
+ECHO.
+ECHO.
+ECHO Program is finished!
+pause
+GOTO :eof
+
+:help
+ECHO Hello. Welcome to TechNobo's Video Transcoder %batVer%, built for generating Proxy files with multitrack audio quickly.
+ECHO.
+ECHO BASIC USAGE:
+ECHO Edit run.bat with a text editor to manually set variables at the top.
+ECHO.
+ECHO INFORMATION OPTIONS:
+ECHO -h, --help            Displays this help INFORMATION
+ECHO.
+ECHO ISSUES RUNNING:
+ECHO - Make sure you're using the most updated Nvidia drivers. The project currently uses NVEncC version %nvenccVer%. Make sure you're using Nvidia graphics driver %minNV% or later.
+goto :eof
+
+:FolderGiven
+    for %%f in (%inF%) do set fnW=%%~nf
+    set outF=%outFLD%\%fnW%%suf%.%outFM%
+goto :fgReturn
+
+:NoFolderGiven
+    FOR %%f IN (%inF%) DO set fn=%%~dpnf
+    set outF=%fn%%suf%.%outFM%
+goto :fgReturn
---
+++ settings.bat
@@ -0,0 +1,163 @@
+:: ----------------------------------------------------
+:: Welcome to TechNobo's Videod Transcoder settings
+:: ----------------------------------------------------
+:: In here is where all the settings are saved.
+:: Edit them here, or set them manually when rendering
+:: files,or by using -s or --settings.
+:: -----------------------------------------------------
+
+:: ----------------------------------------------------
+:: --------------------- REQUIRED ---------------------
+:: ----------------------------------------------------
+:: Output format
+set outFM=mp4
+
+:: Ouput suffix
+:: What goes after the file, just before the extension
+:: set suf=_Proxy >> eg: test_Proxy.mp4
+set suf=_Proxy
+
+:: Output directory
+:: Leave blank to save to the input file's folder.
+:: MAKE SURE IT DOES NOT HAVE A TRAILING \ 
+:: EG: D:\Proxies
+set outFLD=
+
+:: Resolution of the output file.
+:: eg. <640x360/1920x1080 etc> 
+set res=640x360
+
+:: FPS of the output file.
+:: set fps=<24/30/60/120/144 etc>
+set fps=60
+
+:: Codec of the output file.
+:: set codec=<h264/h265>
+:: To see what arguments are available, enter either x32 or x64, and run "nvencc --check-formats" or "NVEncC64 --check-formats"
+set codec=h264
+
+:: Output profile
+:: H.264: baseline, main, high(default), high444
+:: HEVC : main, main10, main444
+:: set profile=<baseline/high/main10 etc>
+set profile=high
+
+:: Output level
+:: - H.264: auto(default), 1, 1b, 1.1, 1.2, 1.3, 2, 2.1, 2.2, 3, 3.1, 3.2, 4, 4.1, 4.2, 5, 5.1, 5.2
+:: - HEVC:  auto(default), 1, 2, 2.1, 3, 3.1, 4, 4.1, 5, 5.1, 5.2, 6, 6.1, 6.2
+:: set level=<auto/1/4.2/5.1 etc>
+set level=auto
+
+:: Encoder preset
+:: set preset=<default/performance/quality>
+set preset=performance
+
+:: Output depth
+:: set outdp=<8/10>
+set outdp=8
+
+:: CUDA Schedule
+:: set cuda schedule mode (default: sync).
+:: auto  : let cuda driver to decide
+:: spin  : CPU will spin when waiting GPU tasks, will provide highest performance but with high CPU utilization.
+:: yield : CPU will yield when waiting GPU tasks.
+:: sync  : CPU will sleep when waiting GPU tasks, performance might drop slightly, while CPU utilization will be lower, especially on HW decode mode.
+set cudasch=spin
+
+:: Input decode to Hardware or Software
+:: avhw: uses libavformat + cuvid for input, this enables full hw transcode and resize.
+:: avhw <native/cuda>       Native is default, I find it faster.
+:: avsw: uses avcodec + sw decoder, dedcoding input file with software.
+::
+:: set hwsw=<avhw <native/cuda> / avsw>
+set decodemode=avhw native
+
+:: Sample Aspect Ratio
+:: set sar=<int>:<int>
+:: Leave blank if you won't use it.
+set sar=1:1
+
+:: Lookahead
+:: Enable lookahead and set lookahead depth (1-32). Default is 16 frames.
+set lookahead=16
+
+:: GOP Length
+:: Default 0 frames
+set GOPLen=0
+
+:: Number of Consecutive B-Frames
+:: -h264 default: 3
+:: -HEVC default: 0
+set bframes=3
+
+:: Number of Reference frames
+:: Default 3 frames
+set ref=3
+
+:: MV Precision
+:: Default: auto
+:: Can also be set to:
+:: - Q-pel (High Quality)
+:: - half-pel
+:: - full-pel (Low Quality)
+set mvpr=Q-pel
+
+:: Colormatrix
+:: Options: undef, auto, bt709, smpte170m, bt470bg, smpte240m, YCgCo, fcc, GBR, bt2020nc, bt2020c
+:: cm=<string>
+set cm=undef
+
+:: --------------------------
+:: ------- h264 ONLY -------
+:: --------------------------
+:: MAKE SURE TO TURN THESE OFF IF YOU'RE NOT USING h264
+:: --------------------------
+:: Use CABAC
+:: ON: cabac=1
+:: OFF: cabac=
+set cabac=1
+
+:: Deblock filter
+:: ON: deblock: on
+:: OFF: deblock: off
+:: If you're not using h264, leave it blank!
+set deblock=on
+
+:: --------------------------
+
+:: --------------------------
+:: ------ VBR SETTINGS ------
+:: --------------------------
+:: Set the output bitrate/quality
+:: cqp <int>                encode in Constant QP Mode
+:: cqp <int>:<int>:<int>    encode in Constant QP Mode
+:: vbr <int>                set bitrate for VBR mode (kbps)
+:: vbrhq <int>              set bitrate for VBR (High Quality) mode (kbps)
+:: cbr <int>                set bitrate for CBR mode (kbps)
+:: cbrhq <int>              set bitrate for CBR (High Quality) mode (kbps)
+set bitrate=cqp 15
+
+:: Set VBR quality (LEAVE BLANK IF NOT USING VRB ABOVE!)
+:: target quality for VBR mode (0-51, 0=auto)
+:: set vbrq=<NOTHING/0-51>
+set vbrq=
+:: Set Max Bitrate in kbps
+:: set maxbr=<int>
+set maxbr=
+:: --------------------------
+
+
+
+
+:: ----------------------------------------------------
+:: --------------------- OPTIONAL ---------------------
+:: ----------------------------------------------------
+:: set bit=<32/64>    -- Chooses to use 32 or 64 bit Windows. Detects automatically if not set.
+set bit=64
+
+:: Other arguments. Will be appended to the end of command that is run.
+:: nvencc -arg -arg -arg -[Added Here]
+:: eg. set otherargs=--interlace tff --crop 20,20,20,20 
+::
+:: To see what arguments are available, enter either x32 or x64, and run "nvencc --help" or "NVEncC64 -h"
+set otherargs=
